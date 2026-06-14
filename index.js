5159bacb2a5c00d0e8511c40da197aa8676f67fc
BlackPantherJazz <henocmontes29@gmail.com>
2026-06-14 01:03:57 -0400

    Add POST, PATCH, DELETE route for user

--- index.js
+++ index.js
@@ -39,6 +39,38 @@ app.get('/api/posts/:id', (req, res) => {
   }
 });
 
+// POST - Create a new user
+app.post('/api/users', (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name,
+        email: req.body.email
+    };
+    users.push(newUser);
+    res.status(201).json(newUser)
+});
+
+// Patch - Update a user
+app.patch('/api/users/:id', (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id));
+    if (!user) {
+        return res.status(404).json({ message: "User not found"});
+    }
+    if (req.body.name) user.name = req.body.name;
+    if (req.body.email) user.email = req.body.email;
+    res.json(user);
+});
+
+// DELETE - Remove a user
+app.delete('/api/users/:id', (req, res) => {
+    const index = users.findIndex(u => u.id === parseInt(req.params.id));
+    if (index === -1) {
+        return res.status(404).json({ message: "User not found"});
+    }
+    const deleted = users.splice(index, 1);
+    res.json({ message: "User Deleted", user: deleted[0]});
+});
+
     // Start the server
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
